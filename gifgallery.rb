d648f566d234fea88c01e4ed551aad17a0577520
Kim Foale <kim@alliscalm.net>
2017-05-20 19:11:57 +0100

    Add proper html view for older phones

--- gifgallery.rb
+++ gifgallery.rb
@@ -2,13 +2,16 @@ require 'sinatra'
 require 'json'
 require 'socket'
 
+# Set server IP dynamically presuming we're on the first 192.168.1.* connection
+$SERVER = Socket.ip_address_list.map{ |n| n.ip_address}.select{ |n| n =~ /192\.168\.[0-1]\.[0-9]*/ }.first + ":4567"
+$DIR = Dir.pwd
+puts "Running on #{$SERVER} at #{$DIR}"
+
 def load_images
-  Dir["./public/*"].select {|x| x =~ /.*(gif)/ }
+  Dir["#{$DIR}/public/*"].select {|x| x =~ /.*\.(gif)/ }
 end
 
-# Set server IP dynamically presuming we're on the first 192.168.1.* connection
-$SERVER = Socket.ip_address_list.map{ |n| n.ip_address}.select{ |n| n =~ /192\.168\.1\.[0-9]*/ }.first + ":4567"
-puts "Running on #{$SERVER}"
+# Global variables to track state
 $images = load_images
 $pointer = 0
 
@@ -19,7 +22,7 @@ def getNextImage
   video = File.basename($images[$pointer])
   extension = File.extname($images[$pointer])
   $pointer += 1
-  { pointer: $pointer, video: "#{$SERVER}/#{video}", fileType: extension }
+  { pointer: $pointer, video: "http://#{$SERVER}/#{video}", fileType: extension }
 end
 
 get '/' do
@@ -27,9 +30,10 @@ get '/' do
 end
 
 # Alternate method to consider
-# get '/image' do
-#   send_file(getNextImage[:video], :disposition => 'inline')
-# end
+get '/image' do
+  file = getNextImage[:video]
+  erb :image, locals: { file: file }
+end
 
 get '/image.json' do
   content_type :json
@@ -38,4 +42,5 @@ end
 
 get '/reload' do
   $images = load_images
+  $images.join(", <br>")
 end
